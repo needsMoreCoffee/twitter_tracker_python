ee904673ae390b685d7e7bef2b8849a0cabe1fbd
ryan <mail@interwebz.space>
2016-09-28 14:47:30 -0600

    Got the script cleaned up and the browser opening to view the webpage but the html bits are missing and only one tweet appears

--- twit.py
+++ twit.py
@@ -16,31 +16,37 @@ from mylogins import twitter
 
 def authit():
 # check if all ready authed and then ask for pin if not otherwise get more authed data.
-    request_token, request_token_secret = twitter.get_request_token()
-    authorize_url = twitter.get_authorize_url(request_token)
-
-    session = twitter.get_auth_session(request_token,
-                            request_token_secret,
-                            method='POST',
-                            data={'oauth_verifier': pin})
-
-    params = {  # Include retweets
-              'count': 10, # 10 tweets
-              'q': '"ufo"',
-              'lang': 'en'} #string to search
-
-    if not session:
-
+    pin = 0
+    if pin == 0:
+        request_token, request_token_secret = twitter.get_request_token()
+        authorize_url = twitter.get_authorize_url(request_token)
 
         print 'Visit this URL in your browser: ' + authorize_url
         #This opens firefox-esr new window for the PIN code
         subprocess.call(["firefox-esr --new-window " + authorize_url], shell=True)
         pin = raw_input('Enter PIN from browser: ')  # `input` if using Python 3!
 
+        session = twitter.get_auth_session(request_token,
+                                request_token_secret,
+                                method='POST',
+                                data={'oauth_verifier': pin})
+
+        params = {  # Include retweets
+                  'count': 10, # 10 tweets
+                  'q': '"ufo"',
+                  'lang': 'en'} #string to search
+
+        r = session.get('search/tweets.json', params=params)
+
     else:
         r = session.get('search/tweets.json', params=params)
 
     digdata(r)
+
+def killunicode(tweetdata):
+    tweetdata = [str(unicodes.encode("ascii", "ignore")) for unicodes in tweetdata]
+    return tweetdata
+
 
 def digdata(authitdata):
 #Start sorting the data here and loading it into strings
@@ -48,12 +54,15 @@ def digdata(authitdata):
     for tweet in authitdata.json()['statuses']:
         #handle = tweet['user']['screen_name']
         #text = tweet['text']
+        killunicode(tweet)
+        tweets = count, str(')' + tweet['user']['screen_name']) + ' - ' + tweet['text'] + "<br>"
         datastring = []
-        datastring.append = count, ')' + tweet['user']['screen_name'] + ' - ' + tweet['text'] + "<br>"
+        datastring.append(tweets)
         count = count + 1
         #print count, ')' + tweet['user']['screen_name'] + ' - ' + tweet['text']
         #print '\n'
     webpage(datastring)
+
 
 def webpage(ourdata):
 
@@ -63,7 +72,7 @@ def webpage(ourdata):
     <!-- refreash the page in the browser over and over to load our new data every few minutes-->
     <meta http-equiv="refresh" content="3" >
     </head>
-    <body>''' + "<center><p>" + ourdata + "</p></center>" + '</body> </html>'
+    <body>''' + "<center><p>" + str(ourdata) + "</p></center>" + '</body> </html>'
 
     htmlfile = open("index.html","w")
     htmlfile.write(html_str)
@@ -80,7 +89,7 @@ def webpage(ourdata):
         pass
 
 def openpage():
-     subprocess.call(["firefox-esr --new-tab" + index.html], shell=True)
+     subprocess.call(["firefox-esr --new-tab" + "index.html"], shell=True)
 
 def main():
     authit()
